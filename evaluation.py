2164ab872cbed9dd9ce0f982a51216cfe9a6277e
Peter Barnett <>
2021-08-17 20:56:59 +1200

    adding evaluation things, and plots of grid searches

--- evaluation.py
+++ evaluation.py
@@ -33,6 +33,18 @@ def generate_history(n_history=100, n_turn=None):
         
     return history, n_turn
 
+
+def generate_history_no_history():
+
+        
+    history = np.zeros((2,2))
+
+    history[0, np.random.randint(2)] = 1
+    
+    history[1, np.random.randint(2)] = 1
+        
+    return history
+
 def is_t4t(agent, n_samples):
     
     agent_actions = []
@@ -40,20 +52,43 @@ def is_t4t(agent, n_samples):
     
     for i in range(n_samples):
         history, n_turn = generate_history()
-        action = agent.compute_action(history)
+        action = agent.compute_action(history.flatten())
         if n_turn == 0:
             opponents_last_action = 0
         else:
             # opponents_last_action = np.where(history[1,:,n_turn-1])
-            opponents_last_action = np.where(history[1,:,0])
+            opponents_last_action = int(np.where(history[1,:,0])[0])
             
         agent_actions.append(action)
         t4t_actions.append(opponents_last_action)
         
     same_action = [int(agent_actions[i]==t4t_actions[i]) for i in range(n_samples)]
-    t4t_fraction = np.sum(same_action)/n_samples
+    t4t_fraction = np.sum(same_action)/n_samples,
+    defect_fraction = np.sum([int(agent_action) for agent_action in agent_actions])/n_samples
+    coop_fraction = 1 - defect_fraction
     
-    return t4t_fraction
+    return t4t_fraction, coop_fraction
+
+def is_t4t_no_history(agent, n_samples):
+    
+    agent_actions = []
+    t4t_actions = []
+    
+    for i in range(n_samples):
+        history = generate_history_no_history()
+        action = agent.compute_action(history.flatten())
+
+        opponents_last_action = int(np.where(history[1,:])[0])
+            
+        agent_actions.append(action)
+        t4t_actions.append(opponents_last_action)
+        
+    same_action = [int(agent_actions[i]==t4t_actions[i]) for i in range(n_samples)]
+    t4t_fraction = np.sum(same_action)/n_samples,
+    defect_fraction = np.sum([int(agent_action) for agent_action in agent_actions])/n_samples
+    coop_fraction = 1 - defect_fraction
+    
+    return t4t_fraction, coop_fraction
     
 
 class test_t4t_agent():
@@ -65,14 +100,34 @@ class test_t4t_agent():
     
     
     def compute_action(self, history):
+
+        # if (not history.shape[0] == 2) or (not history.shape[1]==2):
+        history = np.reshape(history, (2,2,-1))
+            # print('reshaping')
         
         if sum(history[1, :, 0]) == 0:
             action = 0
         else:
-            action = np.where(history[1, :, 0])
+            action = np.where(history[1, :, 0])[0]
             
+        action = int(action)
         if np.random.rand() < self.flip_frac:
             action = int(not action)
+        
+        return action
+        
+
+class test_random_agent():
+    
+    def __init__(self, n_history=100, coop_frac=0.5):
+        
+        self.n_history = n_history
+        self.coop_frac = coop_frac
+    
+    
+    def compute_action(self, history):
+        
+        action = int(np.random.rand() > self.coop_frac)
         
         return action
         
